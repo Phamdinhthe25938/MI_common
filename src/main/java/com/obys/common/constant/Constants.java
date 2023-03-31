750f886a175e3e0bf9c2b9534ee557acdc290005
the.phamdinh <the.phamdinh@vti.com.vn>
2023-03-31 17:11:08 +0700

    add

--- src/main/java/com/obys/common/constant/Constants.java
+++ src/main/java/com/obys/common/constant/Constants.java
@@ -1,6 +1,18 @@
 package com.obys.common.constant;
 
+import java.time.LocalTime;
+import java.util.Date;
+
 public class Constants {
+
+  public final class TimeRegulations {
+    public final static long TIME_OFFICE = 8;
+    public final static LocalTime WORK_START_TIME = LocalTime.of(8, 30, 0);
+    public final static LocalTime WORK_END_TIME = LocalTime.of(17, 30, 0);
+
+  }
+
+  public final static long TIME_OFFICE = 8;
 
   public final class UrlRole {
     public static final String ROLE_ADMIN = "/api/admin";
